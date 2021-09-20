cfcf0dc2f0464a98209485d842b6252c4b768717
mreynier <maxime.rnr@gmail.com>
2021-09-20 11:23:58 +0200

    More Angular Approach to set element's properties.
    Retrieve element reference using @ViewChild Decorator.
    Override default web component properties using VidalSideEffect types.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewContainerRef, ViewRef} from '@angular/core';
 import {VidalSideEffect} from '@vidal-community/vidal-side-effect';
 
 @Component({
@@ -6,14 +6,16 @@ import {VidalSideEffect} from '@vidal-community/vidal-side-effect';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
   title = 'demo-integration-web-component';
 
+  @ViewChild('vse')
+  vidalSideEffectElementRef : ElementRef;
+
   ngAfterViewInit(): void {
-    const vidalSideEffectElement = document.querySelector('vidal-side-effect') as VidalSideEffect;
+    const vidalSideEffectElement = this.vidalSideEffectElementRef.nativeElement as VidalSideEffect;
     vidalSideEffectElement.sideEffectsSupplier = (url) => fetch('http://localhost:8088' + url);
     vidalSideEffectElement.drugIds = ['vidal://product/19649'];
   }
-
 
 }
