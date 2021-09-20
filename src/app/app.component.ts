d0ad69c26dcc77f4f64cde9671cf1d95c3ac654e
mreynier <maxime.rnr@gmail.com>
2021-09-20 11:16:20 +0200

    Vanilla JS Approach to set element's properties.
    Retrieve element reference using querySelector.
    Override default web component properties using VidalSideEffect types.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {VidalSideEffect} from '@vidal-community/vidal-side-effect';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'demo-integration-web-component';
+
+  ngAfterViewInit(): void {
+    const vidalSideEffectElement = document.querySelector('vidal-side-effect') as VidalSideEffect;
+    vidalSideEffectElement.sideEffectsSupplier = (url) => fetch('http://localhost:8088' + url);
+    vidalSideEffectElement.drugIds = ['vidal://product/19649'];
+  }
+
+
 }
