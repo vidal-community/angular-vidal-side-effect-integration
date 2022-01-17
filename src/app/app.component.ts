2289f500f54c48b04acfa1fee73394a1c1506efd
mreynier <maxime.rnr@gmail.com>
2022-01-17 16:24:06 +0100

    Update VWC to 1.3.0 and Add Side Effect component
    Co-authored-by: Jean-Marc Puéchavy <jean-marc.puechavy@vidal.fr>
    Co-authored-by: Nicolas Kosinski <nicolas.kosinski@vidal.fr>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {VidalRecoClic} from '@vidal-community/vidal-web-components';
-import {OutPutProduct} from '@vidal-community/vidal-web-components/vidal-reco-clic/models/out-put-product';
+import {OutputProduct} from '@vidal-community/vidal-web-components/vidal-reco-clic/models/output-product';
+import {VidalSideEffect} from '@vidal-community/vidal-web-components/vidal-side-effect/vidal-side-effect';
+
 
 @Component({
   selector: 'app-root',
@@ -13,14 +15,21 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('vrc')
   vidalRecoClicElementRef: ElementRef;
 
+  @ViewChild('vse')
+  vidalSideEffectElementRef: ElementRef;
+
   ngAfterViewInit(): void {
     const vidalRecoClicElement = this.vidalRecoClicElementRef.nativeElement as VidalRecoClic;
     vidalRecoClicElement.dataFetchingSupplier = (url) => fetch('//recoclic.dockersw2.vidal.fr' + url);
     vidalRecoClicElement.recoId = 1478;
     vidalRecoClicElement.outputProductEvent = this.printOutput;
+
+    const vidalSideEffectElement = this.vidalSideEffectElementRef.nativeElement as VidalSideEffect;
+    vidalSideEffectElement.sideEffectsSupplier = (url) => fetch('//recoclic.dockersw2.vidal.fr' + url);
+    vidalSideEffectElement.drugIds = ['vidal://product/18221','vidal://product/19649'];
   }
 
-  printOutput(outputProduct: OutPutProduct): void {
+  printOutput(outputProduct: OutputProduct): void {
     console.log(outputProduct);
   }
 
